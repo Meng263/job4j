d22ea3fb6124ee0255c3702a538ce599971c65f5
Meng263 <Meng263@gmail.com>
2019-03-10 09:04:21 +0500

    1.3. JavaDoc. Code convention[#118239]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,5 +1,18 @@
 package ru.job4j.calculate;
+/**
+*Calculate
+*
+*@author Oleg Sivakov (Meng263@gmail.com)
+@since 08.03.2019
+@version 0.00001
+*/
 public class Calculate {
+	/**
+	*@author Oleg Sivakov (Meng263@gmail.com)
+	*Точка входа в программу
+	*@param arg -arg // передает массив строк который мы указываем при запуске, например из командной строки 
+	*@return void
+	*/
 	public static void main(String[] args) {
 		
 		System.out.println("Hello world");
