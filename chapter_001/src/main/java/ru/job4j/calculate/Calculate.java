67a26aac3e40dabe36fd847e0fdf3df3818fbdca
Meng263 <Meng263@gmail.com>
2019-03-13 10:16:47 +0500

    3.1. Элементарный калькулятор.[#118230]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,32 +1,32 @@
 package ru.job4j.calculate;
 /**
-*Calculate
-*
-*@author Oleg Sivakov (Meng263@gmail.com)
-@since 08.03.2019
-@version 0.00001
-*/
+ *Calculate
+ *
+ *@author Oleg Sivakov (Meng263@gmail.com)
+ @since 08.03.2019
+ @version 0.00001
+ */
 public class Calculate {
 	/**
-	*@author Oleg Sivakov (Meng263@gmail.com)
-	*Точка входа в программу
-	*@param args -args // передает массив строк который мы указываем при запуске, например из командной строки 	
-	*/
+	 *@author Oleg Sivakov (Meng263@gmail.com)
+	 *Точка входа в программу
+	 *@param args -args // передает массив строк который мы указываем при запуске, например из командной строки
+	 */
 	public static void main(String[] args) {
-		
+
 		System.out.println("Hello world");
-		
+
 	}
-	
+
 	/**
-	*Method echo.
-	*@param name Your name.
-	*@return Echo plus your name.
-	*
-	*/	
+	 *Method echo.
+	 *@param name Your name.
+	 *@return Echo plus your name.
+	 *
+	 */
 	public String echo(String name) {
-		return "Echo, echo, echo : " + name;		
+		return "Echo, echo, echo : " + name;
 	}
-	
-	
+
+
 }
