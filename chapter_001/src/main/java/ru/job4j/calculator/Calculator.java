295796e279d48000a5759215c12948f2d3ebe095
Meng263 <Meng263@gmail.com>
2019-03-13 22:13:46 +0500

    3.1. Элементарный калькулятор.[#118230] - добавление java-docs

--- chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,17 +1,47 @@
 package ru.job4j.calculator;
 
+/**
+ * @author Jora
+ * @version 0.5
+ */
 public class Calculator {
 
+    /**
+     *
+     * @param first - первый аргумент
+     * @param second - второй аргумент
+     * @return - возвращает сумму
+     */
     public double add(double first, double second) {
         return first + second;
     }
 
+    /**
+     *
+     * @param first - первый аргумент
+     * @param second - второй аргумент
+     * @return - возвращает разность
+     */
     public double subtract(double first, double second) {
         return first - second;
     }
+
+    /**
+     *
+     * @param first - первый аргумент
+     * @param second - второй аргумент
+     * @return - возвращает произведение
+     */
     public double multiply(double first, double second) {
         return first * second;
     }
+
+    /**
+     *
+     * @param first - первый аргумент
+     * @param second - второй аргумент
+     * @return - возвращает частное
+     */
     public double div(double first, double second) {
         return first / second;
     }
