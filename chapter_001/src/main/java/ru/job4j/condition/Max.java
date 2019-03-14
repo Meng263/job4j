a163eac5afc13a3ac1e135ab0109b59e09bc11cd
Meng263 <Meng263@gmail.com>
2019-03-14 17:29:22 +0500

    4.2. Максимум из двух чисел[#118203]

---
+++ chapter_001/src/main/java/ru/job4j/condition/Max.java
@@ -0,0 +1,17 @@
+package ru.job4j.condition;
+
+/**
+ * Максимум 2 чисел
+ */
+public class Max {
+    /**
+     *
+     * @param left 1 число
+     * @param right 2 число
+     * @return максимум
+     */
+    public int max(int left, int right) {
+        return  left > right ? left: right;  // тернарный оператор ? :
+
+    }
+}
