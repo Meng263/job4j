85c316139e8a410cbb93e578c267035e71d5f24c
Meng263 <Meng263@gmail.com>
2019-03-14 12:24:42 +0500

    3.4. Расстояние между точками в системе координат[#118231]

---
+++ chapter_001/src/main/java/ru/job4j/point/Point.java
@@ -0,0 +1,18 @@
+package ru.job4j.point;
+
+/**
+ * Вычисление расстояния между двумя точками на координатной плоскости
+ */
+public class Point {
+    /**
+     *
+     * @param x1 координата оси абсцисс 1 точки
+     * @param y1 координата оси ординат 2 точки
+     * @param x2 координата оси абсцисс 2 точки
+     * @param y2 координата оси ординат 2 точки
+     * @return расстояние по модулю
+     */
+    public double distance(int x1, int y1, int x2, int y2) {
+        return Math.sqrt((Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2)));
+    }
+}
