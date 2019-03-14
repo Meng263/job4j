b356c37d3cf517513a24d237c1b99f3d3b6eaad7
Meng263 <Meng263@gmail.com>
2019-03-14 21:49:48 +0500

    4.2. Максимум из двух чисел[#118203] обратный тест

--- chapter_001/src/test/java/ru/job4j/condition/MaxTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/MaxTest.java
@@ -11,4 +11,10 @@ public class MaxTest {
         int result = max.max(1, 2);
         assertThat(result, is(2));
     }
+    @Test
+    public void whenMax2To1Then1() {
+        Max max = new Max();
+        int result = max.max(2, 1);
+        assertThat(result, is(2));
+    }
 }
