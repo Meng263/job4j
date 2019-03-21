9967e043aeb9b9a1e5b6f74d4f74bef941e52228
Meng263 <Meng263@gmail.com>
2019-03-21 21:07:14 +0500

    Пазлы. Тестовое задание.[#118245]

--- chapter_001/src/test/java/ru/job4j/array/ArrayDuplicateTest.java
+++ chapter_001/src/test/java/ru/job4j/array/ArrayDuplicateTest.java
@@ -2,8 +2,8 @@ package ru.job4j.array;
 
 import org.junit.Test;
 
-import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
+import static org.hamcrest.collection.IsArrayContainingInAnyOrder.arrayContainingInAnyOrder;
 
 public class ArrayDuplicateTest {
     @Test
@@ -11,14 +11,14 @@ public class ArrayDuplicateTest {
        ArrayDuplicate arrDup = new ArrayDuplicate();
         String[] arr = new String[]{"Жора", "123", "Java", "123", "Java"};
         String[] result = new String[]{"Жора", "Java", "123"};
-        assertThat(result, is(arrDup.remove(arr)));
+        assertThat(result, arrayContainingInAnyOrder(arrDup.remove(arr)));
     }
 
     @Test
     public void whenRemoveDuplicatesThenArrayWithoutDuplicateSecondT() {
        ArrayDuplicate arrDup = new ArrayDuplicate();
         String[] arr = new String[]{"Жора", "123", "Java", "123", "Java", "123", "Java"};
-        String[] result = new String[]{"Жора", "Java", "123"};
-        assertThat(result, is(arrDup.remove(arr)));
+        String[] result = new String[]{"123", "Java", "Жора"};
+        assertThat(result, arrayContainingInAnyOrder(arrDup.remove(arr)));
     }
 }
