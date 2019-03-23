5868abbcc2ca3b22d5ab2cf0d9d9947da7e257e6
Meng263 <Meng263@gmail.com>
2019-03-23 10:44:33 +0500

    Пазлы. Тестовое задание. #118245 - исправлено 2

--- puzzle/src/main/java/ru/job4j/puzzle/Logic.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Logic.java
@@ -82,10 +82,11 @@ public class Logic {
             for (int j = 0; j < size; j++) {
                 if (table[i][j] == 1) rowCount++;
                 if (table[j][i] == 1) colomnCount++;
-                if ((rowCount == size) || (colomnCount == size)) {
-                    result = true;
-                   return result;
+
                 }
+            if ((rowCount == size) || (colomnCount == size)) {
+                result = true;
+                break;
             }
             rowCount = 0;
             colomnCount = 0;
