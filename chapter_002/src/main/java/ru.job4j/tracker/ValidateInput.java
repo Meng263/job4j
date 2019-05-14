49a9508799568e640d595f52c8a44d3fb0f39ed2
Meng263 <Meng263@gmail.com>
2019-05-14 21:54:18 +0500

    Рефакторинг - Шаблон Декоратор для валидатора.[#118282]

--- chapter_002/src/main/java/ru.job4j/tracker/ValidateInput.java
+++ chapter_002/src/main/java/ru.job4j/tracker/ValidateInput.java
@@ -3,14 +3,23 @@ package ru.job4j.tracker;
 /**
  * Класс реализует корректный ввод данных, обрабатывая исключения
  */
-public class ValidateInput extends ConsoleInput {
+public class ValidateInput implements Input {
+    private final Input input;
+
+    public ValidateInput(final Input input) {
+        this.input = input;
+    }
+    @Override
+    public String ask(String question) {
+        return this.input.ask(question);
+    }
     @Override
     public int ask(String question, int[] range) {
         int result = -1;
         boolean illegal = true;
         do {
             try {
-                result = super.ask(question, range);
+                result = this.input.ask(question, range);
                 illegal = false;
             } catch (MenuOutException moe) {
                 System.out.println("Out of range");
