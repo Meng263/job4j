dc65b2df4fcbd770a9c822a9139a0abd804139a3
Meng263 <Meng263@gmail.com>
2019-05-19 22:22:26 +0500

    Singleton.[#118291] - исправлены тесты

--- chapter_002/src/test/java/ru/job4j/singleton/TrackerSingleTest.java
+++ chapter_002/src/test/java/ru/job4j/singleton/TrackerSingleTest.java
@@ -3,6 +3,7 @@ import org.junit.Test;
 
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 public class TrackerSingleTest {
 
@@ -10,35 +11,27 @@ public class TrackerSingleTest {
     public void whenTrackerEnumIsSingleton() {
         TrackerEnum one = TrackerEnum.INSTANCE;
         TrackerEnum two = TrackerEnum.INSTANCE;
-        assertThat(one, is(two));
-        assertThat(one.equals(two), is(true));
-        assertThat(one.hashCode() == two.hashCode(), is(true));
+        assertTrue(one == two);
     }
 
     @Test
     public void whenTrackerStaticFieldIsSingleton() {
        TrackerStaticField one = TrackerStaticField.getInstance();
         TrackerStaticField two = TrackerStaticField.getInstance();
-        assertThat(one, is(two));
-        assertThat(one.equals(two), is(true));
-        assertThat(one.hashCode() == two.hashCode(), is(true));
+        assertTrue(one == two);
     }
 
     @Test
     public void whenTrackerStaticFinalFieldIsSingleton() {
         TrackerStaticFinalField one = TrackerStaticFinalField.getInstance();
         TrackerStaticFinalField two = TrackerStaticFinalField.getInstance();
-        assertThat(one, is(two));
-        assertThat(one.equals(two), is(true));
-        assertThat(one.hashCode() == two.hashCode(), is(true));
+        assertTrue(one == two);
     }
 
     @Test
     public void whenTrackerStaticFinalClassIsSingleton() {
         TrackerStaticFinalClass one = TrackerStaticFinalClass.getInstance();
         TrackerStaticFinalClass two = TrackerStaticFinalClass.getInstance();
-        assertThat(one, is(two));
-        assertThat(one.equals(two), is(true));
-        assertThat(one.hashCode() == two.hashCode(), is(true));
+        assertTrue(one == two);
     }
 }
