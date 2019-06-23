1dce75f9f2f2f4081fe677ce043416811f061c1d
Meng263 <Meng263@gmail.com>
2019-06-23 19:11:08 +0500

    Кофемашина. [#34741]

---
+++ chapter_002/src/main/java/ru/job4j/coffeeMachine/CoffeeMachine.java
@@ -0,0 +1,45 @@
+package ru.job4j.coffeeMachine;
+
+import java.util.Arrays;
+
+/**
+ * @author Oleg
+ * @since 23.06.2019
+ */
+public class CoffeeMachine {
+    /**
+     *Конструктор по-умолчанию, инициализирует массив с номиналами монет
+     */
+    public CoffeeMachine() {
+        coins = new int[]{1, 2, 5, 10};
+    }
+
+    /**
+     * Массив, содержащий номиналы монет для сдачи
+     */
+    private final int[] coins;
+
+    /**
+     * Метод выдачи сдачи для автомата
+     *
+     * @param value купюра 50р 100р и т д
+     * @param price цена кофе
+     * @return массив монет
+     */
+    int[] changes(int value, int price) {
+        int[] arr = new int[50];
+        int maxCoin = coins[coins.length - 1];
+        int change = value - price;
+        int k = coins.length - 1;
+        int i = 0;
+        while (change != 0) {
+            if (change >= maxCoin) {
+                arr[i++] = maxCoin;
+                change = change - maxCoin;
+            } else {
+                maxCoin = coins[--k];
+            }
+        }
+        return Arrays.copyOf(arr, i);
+    }
+}
---
+++ chapter_002/src/test/java/ru/job4j/coffeeMachine/CoffeeMachineTest.java
@@ -0,0 +1,30 @@
+package ru.job4j.coffeeMachine;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class CoffeeMachineTest {
+
+    @Test
+    public void whenCoffee35Value50thanChangeIs15() {
+        CoffeeMachine coffee = new CoffeeMachine();
+        int[] result = new int[]{10, 5};
+        assertThat(result, is(coffee.changes(50, 35)));
+    }
+
+    @Test
+    public void whenCoffee20Value100thanChangeIs80() {
+        CoffeeMachine coffee = new CoffeeMachine();
+        int[] result = new int[]{10, 10, 10, 10, 10, 10, 10, 10};
+        assertThat(result, is(coffee.changes(100, 20)));
+    }
+
+    @Test
+    public void whenCoffee36Value50thanChangeIs14() {
+        CoffeeMachine coffee = new CoffeeMachine();
+        int[] result = new int[]{10, 2, 2};
+        assertThat(result, is(coffee.changes(50, 36)));
+    }
+}
