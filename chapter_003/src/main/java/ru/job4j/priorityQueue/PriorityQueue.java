f75cf2b805a0db2f5cfb1ed5f4422d7716811526
Meng263 <Meng263@gmail.com>
2019-07-06 15:35:32 +0500

    Очередь с приоритетом на LinkedList[#118263]

---
+++ chapter_003/src/main/java/ru/job4j/priorityQueue/PriorityQueue.java
@@ -0,0 +1,43 @@
+package ru.job4j.priorityQueue;
+
+import java.util.*;
+
+/**
+ * Очередь задач с приоритетами, добавляет новую задачу в нужную позицию в соответсвии с приоритетом.
+ * Извлекается задача с наибольшим приоритетом
+ */
+public class PriorityQueue {
+    private LinkedList<Task> tasks = new LinkedList<>();
+
+    /**
+     * Метод вставляет в нужную позицию элемент.
+     * Позиция определяется по полю приоритет, чем меньшее занчение, тем выше приоритет
+     *
+     * @param task задача
+     */
+    public void put(Task task) {
+        int count = 0;
+        if (tasks.isEmpty()) {
+            tasks.add(task);
+        } else {
+            Iterator<Task> iterator = tasks.listIterator();
+            while (iterator.hasNext()) {
+                Task element = iterator.next();
+                if (element.getPriority() >= task.getPriority()) {
+                    break;
+                }
+                count++;
+            }
+            tasks.add(count, task);
+        }
+    }
+
+    /**
+     * Метод извлекает задачу с наибольшим приорететом
+     *
+     * @return задача
+     */
+    public Task take() {
+        return this.tasks.poll();
+    }
+}
