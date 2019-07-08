e9ee43498b60222ed774d57e6d4f125605eda022
Meng263 <Meng263@gmail.com>
2019-07-08 15:21:14 +0500

    Организовать сортировку User[#118254]

---
+++ chapter_003/src/main/java/ru/job4j/sortedSet/User.java
@@ -0,0 +1,34 @@
+package ru.job4j.sortedSet;
+
+import java.util.Objects;
+
+/**
+ * Пользователь с параметрами возраст и имя
+ */
+public class User implements Comparable<User> {
+    Integer age;
+    String name;
+
+    public User(Integer age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return age == user.age &&
+                Objects.equals(name, user.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(age, name);
+    }
+
+    public int compareTo(User o) {
+        return this.age.compareTo(o.age);
+    }
+}
