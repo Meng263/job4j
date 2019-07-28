6fefc88cf12699dce20530d8c330b1f8cefd3aff
Meng263 <Meng263@gmail.com>
2019-07-28 20:40:09 +0500

    Уникальность элементов и сортировка.[#118168]

--- chapter_004/src/main/java/ru/job4j/listtoaddress/Profiles.java
+++ chapter_004/src/main/java/ru/job4j/listtoaddress/Profiles.java
@@ -1,5 +1,6 @@
 package ru.job4j.listtoaddress;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -14,6 +15,17 @@ public class Profiles {
      */
     List<Address> collect(List<Profile> profiles) {
         List<Address> list = profiles.stream().map(Profile::getAddress).collect(Collectors.toList());
+        return list;
+    }
+
+    /**
+     * Метод преобразует список профилей в список адресов, возвращает только уникальные элементы, отсортированные по городу
+     * @param profiles список профилей
+     * @return список адресов
+     */
+    List<Address> collectUniqueSorted(List<Profile> profiles) {
+        List<Address> list = profiles.stream().map(Profile::getAddress).distinct()
+                .sorted(Comparator.comparing(Address::getCity)).collect(Collectors.toList());
         return list;
     }
 }
--- chapter_004/src/test/java/ru/job4j/listtoaddress/ProfilesTest.java
+++ chapter_004/src/test/java/ru/job4j/listtoaddress/ProfilesTest.java
@@ -34,4 +34,38 @@ public class ProfilesTest {
         assertThat(expected, is(prof.collect(profiles)));
     }
 
+    @Test
+    public void whenListProfileToListAddressSorted() {
+        Address ekt = new Address("Yekaterinburg", "Lenina", 5, 55);
+        Address london = new Address("London", "Baiker", 3, 5);
+        Address parisOne = new Address("Paris", "Elesey", 21, 11);
+        Address parisTwo = new Address("Paris", "Elesey", 13, 1);
+        Address moscow = new Address("Moscow", "Kutuzovskiy", 22, 58);
+        Profile jora = new Profile(ekt);
+        Profile linaOne = new Profile(moscow);
+        Profile lena = new Profile(london);
+        Profile tanja = new Profile(parisOne);
+        Profile gena = new Profile(parisTwo);
+        Profile oleg = new Profile(parisOne);
+        Profile linaTwo = new Profile(moscow);
+
+        List<Profile> profiles = List.of(
+                jora,
+                linaOne,
+                lena,
+                tanja,
+                gena,
+                oleg,
+                linaTwo
+        );
+        List<Address> expected = List.of(
+                london,
+                moscow,
+                parisOne,
+                parisTwo,
+                ekt
+        );
+        Profiles prof = new Profiles();
+        assertThat(expected, is(prof.collectUniqueSorted(profiles)));
+    }
 }
