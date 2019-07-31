70f95419f822c9f3f9fe971fecaa9e27347cfd95
Meng263 <Meng263@gmail.com>
2019-07-31 21:34:36 +0500

    Collection API Улучшения[#118173]

--- chapter_003/src/test/java/ru/job4j/list2map/UserConvertTest.java
+++ chapter_003/src/test/java/ru/job4j/list2map/UserConvertTest.java
@@ -1,9 +1,9 @@
 package ru.job4j.list2map;
 
 import org.junit.Test;
-import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
@@ -11,33 +11,37 @@ public class UserConvertTest {
     @Test
     public void when3ItemThan() {
         UserConvert userConvert = new UserConvert();
-        List<User> list = new LinkedList<>();
         User jora = new User(133, "Jora", "Yekaterinburg");
         User oleg = new User(122, "Oleg", "Moscow");
         User lena = new User(144, "Lena", "Paris");
-        list.add(jora);
-        list.add(oleg);
-        list.add(lena);
-        HashMap<Integer, User> result = userConvert.process(list);
-        HashMap<Integer, User> expected = new HashMap<>();
-        expected.put(133, jora);
-        expected.put(122, oleg);
-        expected.put(144, lena);
+        List<User> list = List.of(
+                jora,
+                oleg,
+                lena
+        );
+        Map<Integer, User> result = userConvert.process(list);
+        Map<Integer, User> expected = Map.of(
+                133, jora,
+                122, oleg,
+                144, lena
+        );
         assertThat(result, is(expected));
     }
 
     @Test
     public void when2ItemThan() {
         UserConvert userConvert = new UserConvert();
-        List<User> list = new LinkedList<>();
         User jora = new User(13, "Jora", "Yekaterinburg");
         User lena = new User(144, "Lena", "Paris");
-        list.add(jora);
-        list.add(lena);
-        HashMap<Integer, User> result = userConvert.process(list);
-        HashMap<Integer, User> expected = new HashMap<>();
-        expected.put(13, jora);
-        expected.put(144, lena);
+        List<User> list = List.of(
+                jora,
+                lena
+        );
+        Map<Integer, User> result = userConvert.process(list);
+        Map<Integer, User> expected = Map.of(
+                13, jora,
+                144, lena
+        );
         assertThat(result, is(expected));
     }
 }
