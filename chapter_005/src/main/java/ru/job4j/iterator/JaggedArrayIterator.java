19e49fb63010d3fc93be7966381b8ee7d15f9bdb
Meng263 <Meng263@gmail.com>
2019-08-10 21:10:20 +0500

    Итератор для двухмерного массива int[][][#167645]

---
+++ chapter_005/src/main/java/ru/job4j/iterator/JaggedArrayIterator.java
@@ -0,0 +1,45 @@
+package ru.job4j.iterator;
+import java.util.Iterator;
+
+/**
+ * Итератор для двумерного массива
+ */
+public class JaggedArrayIterator implements Iterator<java.lang.Integer> {
+
+    public JaggedArrayIterator(int[][] values) {
+        this.values = values;
+    }
+
+    private int[][] values;
+    private int i = 0, j = 0;
+
+    /**
+     * Метод проверяет есть ли следующий элемент
+     * @return
+     */
+    @Override
+    public boolean hasNext() {
+        boolean result = true;
+        if (i >= values.length) {
+            result = false;
+        } else if (j >= values[i].length) {
+            result = false;
+        }
+        return result;
+    }
+
+    /**
+     * Метод возвращает текущий элемент и сдвигает указатель на следующую ячейку
+     * @return
+     */
+    @Override
+    public java.lang.Integer next() {
+        int result = values[i][j];
+        j++;
+        if (j >= values[i].length) {
+            j = 0;
+            i++;
+        }
+        return result;
+    }
+}
