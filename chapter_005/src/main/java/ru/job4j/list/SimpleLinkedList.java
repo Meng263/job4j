942b37b362ff84199c01733e8f334c10f37ba887
Meng263 <Meng263@gmail.com>
2019-08-18 20:10:35 +0500

    Создать метод delete для односвязного списка[#167623]

---
+++ chapter_005/src/main/java/ru/job4j/list/SimpleLinkedList.java
@@ -0,0 +1,80 @@
+package ru.job4j.list;
+
+/**
+ * Класс SimpleLinkedList.
+ */
+public class SimpleLinkedList<E> {
+
+    private int size;
+    private Node<E> first;
+
+
+    /**
+     * Метод возвращает true, если список пуст
+     *
+     * @return true, если список пуст
+     */
+    public boolean isEmpty() {
+        boolean result = false;
+        if (size == 0) {
+            result = true;
+        }
+        return result;
+    }
+
+    /**
+     * Метод вставляет в начало списка данные.
+     */
+    public void add(E data) {
+        Node<E> newLink = new Node<>(data);
+        newLink.next = this.first;
+        this.first = newLink;
+        this.size++;
+    }
+
+    /**
+     * Метод удаляет первый элемент в списке
+     *
+     * @return true, если элемент удален
+     */
+    public boolean delete() {
+        boolean result = false;
+        if (!isEmpty()) {
+            this.first = this.first.next;
+            this.size--;
+            result = true;
+        }
+        return result;
+    }
+
+    /**
+     * Метод получения элемента по индексу.
+     */
+    public E get(int index) {
+        Node<E> result = this.first;
+        for (int i = 0; i < index; i++) {
+            result = result.next;
+        }
+        return result.data;
+    }
+
+    /**
+     * Метод получения размера коллекции.
+     */
+    public int getSize() {
+        return this.size;
+    }
+
+    /**
+     * Класс предназначен для хранения данных.
+     */
+    private static class Node<E> {
+
+        E data;
+        Node<E> next;
+
+        Node(E data) {
+            this.data = data;
+        }
+    }
+}
---
+++ chapter_005/src/test/java/ru/job4j/list/SimpleLinkedListTest.java
@@ -0,0 +1,40 @@
+package ru.job4j.list;
+
+import org.junit.Test;
+import org.junit.Before;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class SimpleLinkedListTest {
+
+    private SimpleLinkedList<Integer> list;
+
+    @Before
+    public void beforeTest() {
+        list = new SimpleLinkedList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+    }
+
+    @Test
+    public void whenAddThreeElementsThenUseGetOneResultTwo() {
+        assertThat(list.get(1), is(2));
+    }
+
+    @Test
+    public void whenAddThreeElementsThenUseGetSizeResultThree() {
+        assertThat(list.getSize(), is(3));
+    }
+
+    @Test
+    public void whenDeleteFirstElementThenSizeResultTwo() {
+        assertThat(list.delete(), is(true));
+        assertThat(list.getSize(), is(2));
+        assertThat(list.get(0), is(2));
+        assertThat(list.get(1), is(1));
+        assertThat(list.delete(), is(true));
+        assertThat(list.delete(), is(true));
+        assertThat(list.delete(), is(false));
+    }
+}
