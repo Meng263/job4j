f49d376102674adafb8fa8156b96084fdee6344e
Meng263 <Meng263@gmail.com>
2019-09-10 20:52:34 +0500

    Читаем файл конфигурации[#167660]

--- chapter_006/src/main/java/ru/job4j/io/Config.java
+++ chapter_006/src/main/java/ru/job4j/io/Config.java
@@ -6,20 +6,48 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.StringJoiner;
 
+/**
+ * Чтение файла конфигурации
+ */
 public class Config {
     private final String path;
-    private final Map<String, String> values = new HashMap<String, String>();
+    private final Map<String, String> values = new HashMap<>();
 
     public Config(final String path) {
         this.path = path;
     }
 
+    /**
+     * Метод загружает строки из файла в map, ключ - строка до разделителя "=",
+     * занчение после разделителя, коментарии игнорируются
+     * Пример: "hibernate.connection.username=postgres" запишется в карту как:
+     * ключ "hibernate.connection.username", значение "postgres"
+     */
     public void load() {
+        try (BufferedReader read = new BufferedReader(new FileReader(this.path))) {
+            read.lines().forEach(line -> {
+                if (!line.isEmpty()
+                        && !line.startsWith("//")
+                        && !line.startsWith("/*")
+                        && !line.startsWith("*")
+                        && !line.startsWith("#")
+                        && !line.startsWith(" ")) {
+                    String[] keyValueArr = line.split("=");
+                    if (keyValueArr.length == 2) {
+                        values.put(keyValueArr[0], keyValueArr[1]);
+                    }
+                }
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
+
     public String value(String key) {
-        throw new UnsupportedOperationException("Don't impl this method yet!");
+        return this.values.get(key);
     }
+
     @Override
     public String toString() {
         StringJoiner out = new StringJoiner(System.lineSeparator());
--- chapter_006/src/test/java/ru/job4j/io/ConfigTest.java
+++ chapter_006/src/test/java/ru/job4j/io/ConfigTest.java
@@ -1,7 +1,17 @@
 package ru.job4j.io;
-
+import org.junit.Test;
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
 
 public class ConfigTest {
-
+        @Test
+    public void whenLoadThanGetActualValue() {
+            Config config = new Config(Config.class.getClassLoader().getResource("app.properties").getFile());
+            config.load();
+            assertThat(config.value("hibernate.dialect"), is("org.hibernate.dialect.PostgreSQLDialect"));
+            assertThat(config.value("hibernate.connection.url"), is("jdbc:postgresql://127.0.0.1:5432/trackstudio"));
+            assertThat(config.value("hibernate.connection.driver_class"), is("org.postgresql.Driver"));
+            assertThat(config.value("hibernate.connection.username"), is("postgres"));
+            assertThat(config.value("hibernate.connection.password"), is("password"));
+        }
 }
