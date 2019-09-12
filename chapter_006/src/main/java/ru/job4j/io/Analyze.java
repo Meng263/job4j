63b1ae38fd4252344ebd361ab65de5d2baf743c1
Meng263 <Meng263@gmail.com>
2019-09-12 15:21:22 +0500

    Анализ доступности сервера.[#167661] - исправлено

--- chapter_006/src/main/java/ru/job4j/io/Analyze.java
+++ chapter_006/src/main/java/ru/job4j/io/Analyze.java
@@ -12,47 +12,70 @@ public class Analyze {
     /**
      * Метод преобразует один файл в другой, на основе лога нахоит интервалы, когда не работал сервер
      * Сервер не работал. если status = 400 или 500.
+     *
      * @param source лог сервера
-     * @param target файл с интервалами, когда сервер не работал в формате 
-     * hh:mm:ss;hh:mm:ss; (начало периода;конец периода;) 
+     * @param target файл с интервалами, когда сервер не работал в формате
+     *               hh:mm:ss;hh:mm:ss; (начало периода;конец периода;)
      */
     public void unavailable(String source, String target) {
-        boolean isStartTimeInterval = false;
-        List<String> sourceLines;
+        List<String> sourceLines = readFromFile(source);
         List<String> targetLines = new ArrayList<>(100);
         String beginTimeInterval = "";
         String endTimeInterval = "";
-        try (BufferedReader read = new BufferedReader(new FileReader(source))) {
-            sourceLines = read.lines().collect(Collectors.toList());
-            read.close();
-            for (String line : sourceLines) {
-                if (!line.isEmpty()
-                        && !line.startsWith("/*")
-                        && !line.startsWith("*")
-                        && !line.startsWith("#")
-                        && !line.startsWith(" ")) {
-                    String[] keyValueArr = line.split(" ");
-                    int code = Integer.parseInt(keyValueArr[0]);
-                    String time = keyValueArr[1];
-                    if (!isStartTimeInterval && (code == 400 || code == 500)) {
-                        isStartTimeInterval = true;
-                        beginTimeInterval = time;
-                    }
-                    if (isStartTimeInterval && code != 400 && code != 500) {
-                        isStartTimeInterval = false;
-                        endTimeInterval = time;
-                        targetLines.add(new StringBuilder()
-                                .append(beginTimeInterval)
-                                .append(";")
-                                .append(endTimeInterval)
-                                .append(";")
-                                .toString());
-                    }
+        boolean isStartTimeInterval = false;
+        for (String line : sourceLines) {
+            if (!line.isEmpty()
+                    && !line.startsWith("/*")
+                    && !line.startsWith("*")
+                    && !line.startsWith("#")
+                    && !line.startsWith(" ")) {
+                String[] keyValueArr = line.split(" ");
+                int code = Integer.parseInt(keyValueArr[0]);
+                String time = keyValueArr[1];
+                if (!isStartTimeInterval && (code == 400 || code == 500)) {
+                    isStartTimeInterval = true;
+                    beginTimeInterval = time;
+                }
+                if (isStartTimeInterval && code != 400 && code != 500) {
+                    isStartTimeInterval = false;
+                    endTimeInterval = time;
+                    targetLines.add(new StringBuilder()
+                            .append(beginTimeInterval)
+                            .append(";")
+                            .append(endTimeInterval)
+                            .append(";")
+                            .toString());
                 }
             }
-            PrintWriter out = new PrintWriter(new FileOutputStream(target));
-            targetLines.forEach(out::println);
-            out.close();
+        }
+        writeToFile(targetLines, target);
+    }
+
+    /**
+     * Метод читает данные из файла, возвращает список строк
+     *
+     * @param filePath путь к файлу
+     * @return список строк
+     */
+    private List<String> readFromFile(String filePath) {
+        List<String> result = null;
+        try (BufferedReader read = new BufferedReader(new FileReader(filePath))) {
+            result = read.lines().collect(Collectors.toList());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    /**
+     * Метод записывает список строк в файл
+     *
+     * @param lines    список строк
+     * @param filePath путь к файлу
+     */
+    private void writeToFile(List<String> lines, String filePath) {
+        try (PrintWriter out = new PrintWriter(new FileOutputStream(filePath))) {
+            lines.forEach(out::println);
         } catch (Exception e) {
             e.printStackTrace();
         }
