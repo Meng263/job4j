d4b94f3f18cccddd8221e2d20ef8125aeef6400d
Meng263 <Meng263@gmail.com>
2019-10-27 22:42:53 +0500

    Архивировать проект. [#167662]

---
+++ chapter_006/src/main/java/ru/job4j/io/Zip.java
@@ -0,0 +1,121 @@
+package ru.job4j.io;
+
+import java.io.*;
+import java.util.*;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+/**
+ * Архивация файлов
+ */
+public class Zip {
+    /**
+     * Метод упаковывает список файлов в зип архив
+     * @param source список файлов
+     * @param target файл архива
+     */
+    private void pack(List<File> source, File target) {
+        try (ZipOutputStream zos = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
+            source.forEach(file -> {
+                if (!file.equals(target)) {
+                    try {
+                        zos.putNextEntry(new ZipEntry(file.getPath()));
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(file))) {
+                        zos.write(out.readAllBytes());
+                        System.out.println("Добавленен в архив файл " + file.getCanonicalPath());
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Метод ищет в заданном каталоге и во воложенных каталогах файлы с расширением
+     * файлы без расширения игнорируются
+     *
+     * @param rootPath путь к каталогу
+     * @param ext      расширение
+     * @return список файлов
+     * @throws IOException в случае, если заданный каталог не является каталогом
+     */
+    private List<File> seekBy(String rootPath, String ext) throws IOException {
+        String[] splitExt = ext.split("\\.");
+        String actualExt = splitExt[splitExt.length - 1];
+        List<File> result = new ArrayList<>();
+        File root = new File(rootPath);
+        if (!root.isDirectory()) {
+            throw new IOException("Not is directory!");
+        }
+        Queue<File> queue = new LinkedList<>(Arrays.asList(Objects.requireNonNull(root.listFiles())));
+        while (!queue.isEmpty()) {
+            File currentFile = queue.poll();
+            if (currentFile.isDirectory()) {
+                queue.addAll(Arrays.asList(Objects.requireNonNull(currentFile.listFiles())));
+            } else {
+                String[] split = currentFile.getName().split("\\.");
+                if (split.length > 0) {
+                    String currentFileExt = split[split.length - 1];
+                    if (!actualExt.equals(currentFileExt)) {
+                        result.add(currentFile);
+                    }
+                }
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Метод запускает программу, параметры нужно указать:
+     *
+     * -d - directory - которую мы хотим архивировать
+     * -e - exclude - исключить файлы *.xml
+     * -o - output - во что мы архивируем.
+     *
+     * @param args массив параметров
+     */
+    public static void main(String[] args) {
+        Zip zip = new Zip();
+        Map<String, String> map = Args.getParam(args);
+        try {
+            zip.pack(zip.seekBy(map.get("-d"), map.get("-e")), new File(map.get("-o")));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Класс содержит статический метод для парсинга параметров запуска
+     */
+    private static class Args {
+        /**
+         * Метод парсит параметры запуска и возвращает в виде Map
+         * @param args параметры запуска
+         * @return Map ключ строка с префиксом "-", значение следующая строка без префикса
+         */
+        private static Map<String, String> getParam(String[] args) {
+            Map<String, String> map = new HashMap<>();
+            String keyTmp = "";
+            String valueTmp = "";
+            for (String param : args) {
+                if (param.startsWith("-")) {
+                    keyTmp = param;
+                } else {
+                    valueTmp = param;
+                }
+                if (!keyTmp.isEmpty() && !valueTmp.isEmpty()) {
+                    map.put(keyTmp, valueTmp);
+                    keyTmp = "";
+                    valueTmp = "";
+                }
+            }
+            return map;
+        }
+    }
+}
---
+++ chapter_006/src/test/java/ru/job4j/io/ZipTest.java
@@ -0,0 +1,63 @@
+package ru.job4j.io;
+
+import org.junit.Test;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class ZipTest {
+    @Test
+    public void whenPackFilesThanThisFilesContainsInZipFile() {
+        List<File> result = new ArrayList<>();
+        List<File> expected = new ArrayList<>();
+        String rootPath = System.getProperty("java.io.tmpdir");
+        File root = new File(rootPath, "root");
+        root.mkdir();
+        File firstChild = new File(root, "first");
+        firstChild.mkdir();
+        File secondChild = new File(firstChild, "second");
+        secondChild.mkdir();
+        try {
+            for (int i = 0; i < 5; i++) {
+                File file = new File(root, i + ".txt");
+                file.createNewFile();
+                expected.add(file);
+            }
+            for (int i = 0; i < 3; i++) {
+                File file = new File(firstChild, i + ".log");
+                file.createNewFile();
+                expected.add(file);
+            }
+            for (int i = 0; i < 2; i++) {
+                File file = new File(secondChild, i + ".odt");
+                file.createNewFile();
+                expected.add(file);
+                file = new File(secondChild, i * 3 + ".doc");
+                file.createNewFile();
+            }
+            Zip.main(new String[]{"-d", System.getProperty("java.io.tmpdir") + File.separator + "root",
+                   "-e", "*.doc", "-o", System.getProperty("java.io.tmpdir") + File.separator + "project.zip"});
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        try (ZipInputStream zis = new ZipInputStream(new FileInputStream(System.getProperty("java.io.tmpdir") + File.separator + "project.zip"))) {
+            ZipEntry entry;
+            String fileName;
+            while ((entry = zis.getNextEntry()) != null) {
+                fileName = entry.getName();
+                result.add(new File(fileName));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        assertThat(result, is(expected));
+    }
+}
