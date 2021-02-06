22ae7cc9e26c0b4303d81ed5f3aaf2e45221054c
Oleg Sivakov <Meng@siams.com>
2021-02-06 16:52:22 +0500

    fix test for CI

--- chapter_008/src/test/java/ru/job4j/solid/srp/report/JsonReportTest.java
+++ chapter_008/src/test/java/ru/job4j/solid/srp/report/JsonReportTest.java
@@ -5,6 +5,10 @@ import org.junit.Test;
 import ru.job4j.solid.srp.MemStore;
 import ru.job4j.solid.srp.MemStoreGenerator;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
 public class JsonReportTest {
     @Test
     public void whenGenerateJsonShouldBeCorrectJson() {
@@ -12,12 +16,17 @@ public class JsonReportTest {
         MemStore store = generator.getStore();
         JsonReport engine = new JsonReport(store);
         String generated = engine.generate(employee -> employee.getName().contains("Ivan"));
-        String expected = "[ {\n" +
-                "  \"fired\" : \"2021-02-03\",\n" +
-                "  \"hired\" : \"2021-02-03\",\n" +
+        String expected = String.format("[ {\n" +
+                "  \"fired\" : \"%s\",\n" +
+                "  \"hired\" : \"%s\",\n" +
                 "  \"name\" : \"Ivan\",\n" +
                 "  \"salary\" : 100.0\n" +
-                "} ]";
+                "} ]", localDateToString(LocalDateTime.now()), localDateToString(LocalDateTime.now()));
         Assert.assertEquals(generated, expected);
+    }
+
+    private String localDateToString(LocalDateTime localDateTime) {
+        DateTimeFormatter format = DateTimeFormatter.ofPattern("yyyy-MM-dd", Locale.getDefault());
+        return format.format(localDateTime);
     }
 }
--- chapter_008/src/test/java/ru/job4j/solid/srp/report/XmlReportTest.java
+++ chapter_008/src/test/java/ru/job4j/solid/srp/report/XmlReportTest.java
@@ -5,6 +5,10 @@ import org.junit.Test;
 import ru.job4j.solid.srp.MemStore;
 import ru.job4j.solid.srp.MemStoreGenerator;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
 public class XmlReportTest {
     @Test
     public void whenGenerateJsonShouldBeCorrectJson() {
@@ -12,12 +16,17 @@ public class XmlReportTest {
         MemStore store = generator.getStore();
         XmlReport engine = new XmlReport(store);
         String generated = engine.generate(employee -> employee.getName().contains("Ivan"));
-        String expected = "<ArrayList><item>" +
-                "<fired>2021-02-03</fired>" +
-                "<hired>2021-02-03</hired>" +
+        String expected = String.format("<ArrayList><item>" +
+                "<fired>%s</fired>" +
+                "<hired>%s</hired>" +
                 "<name>Ivan</name>" +
                 "<salary>100.0</salary>" +
-                "</item></ArrayList>";
+                "</item></ArrayList>", localDateToString(LocalDateTime.now()), localDateToString(LocalDateTime.now()));
         Assert.assertEquals(generated, expected);
+    }
+
+    private String localDateToString(LocalDateTime localDateTime) {
+        DateTimeFormatter format = DateTimeFormatter.ofPattern("yyyy-MM-dd", Locale.getDefault());
+        return format.format(localDateTime);
     }
 }
